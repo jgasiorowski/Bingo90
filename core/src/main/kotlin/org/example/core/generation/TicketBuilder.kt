2457271e78a2a68d1511b30991ea5e32886cbcb0
Jacek Gasiorowski <gasiorowski.j@gmail.com>
2025-02-09 19:11:30 +0100

    Refactor multiple if statements

--- core/src/main/kotlin/org/example/core/generation/TicketBuilder.kt
+++ core/src/main/kotlin/org/example/core/generation/TicketBuilder.kt
@@ -24,43 +24,45 @@ class TicketBuilder(val index: Int) {
 
     private fun balanceEmptyCells() {
         val rowsEmptyCells = Array(3) { RowEmptyCells(it) }
+        fun arrangeColumnWithTwoEmptyCells(column: Column){
+            val rowIndicesLeft = mutableListOf(0, 1, 2)
+            val ordered = rowsEmptyCells.sortedByDescending { c -> c.count }
+            val columnIndex = column.index
 
-        for (column in columns.sortedBy { r -> r.size }) {
-            if (column.size == 1) {
-                val rowIndicesLeft = mutableListOf(0, 1, 2)
-                val ordered = rowsEmptyCells.sortedByDescending { c -> c.count }
-                val columnIndex = column.index
-
-                (0..1).map {
-                    val row = ordered[it]
-                    rowIndicesLeft.remove(row.index)
-                    row.count--
-                }
-
-                rows[rowIndicesLeft.first()][columnIndex] = column.first()
-            }
-
-            if (column.size == 2) {
-                val columnIndicesLeft = mutableListOf(0, 1, 2)
-                val row = rowsEmptyCells.maxByOrNull { c -> c.count }!!
-                val columnIndex = column.index
-
-                columnIndicesLeft.remove(row.index)
+            (0..1).map {
+                val row = ordered[it]
+                rowIndicesLeft.remove(row.index)
                 row.count--
-
-                val ordered = column.sortedBy { it }
-                rows[columnIndicesLeft.first()][columnIndex] = ordered[0]
-                rows[columnIndicesLeft.last()][columnIndex] = ordered[1]
-
             }
 
-            if (column.size == 3) {
-                val columnIndex = column.index
-                val ordered = column.sortedBy { it }
+            rows[rowIndicesLeft.first()][columnIndex] = column.first()
+        }
+        fun arrangeColumnWithOneEmptyCell(column: Column){
+            val columnIndicesLeft = mutableListOf(0, 1, 2)
+            val row = rowsEmptyCells.maxByOrNull { c -> c.count }!!
+            val columnIndex = column.index
 
-                rows[0][columnIndex] = ordered[0]
-                rows[1][columnIndex] = ordered[1]
-                rows[2][columnIndex] = ordered[2]
+            columnIndicesLeft.remove(row.index)
+            row.count--
+
+            val ordered = column.sortedBy { it }
+            rows[columnIndicesLeft.first()][columnIndex] = ordered[0]
+            rows[columnIndicesLeft.last()][columnIndex] = ordered[1]
+        }
+        fun arrangeColumnWithNoEmptyCells(column: Column){
+            val columnIndex = column.index
+            val ordered = column.sortedBy { it }
+
+            rows[0][columnIndex] = ordered[0]
+            rows[1][columnIndex] = ordered[1]
+            rows[2][columnIndex] = ordered[2]
+        }
+
+        for (column in columns.sortedBy { c -> c.size }) {
+            when (column.size){
+                1 -> arrangeColumnWithTwoEmptyCells(column)
+                2 -> arrangeColumnWithOneEmptyCell(column)
+                3 -> arrangeColumnWithNoEmptyCells(column)
             }
         }
     }
